f6a933a93b352a6b9caa9008aba35af25217a230
RodriHaro <rodri.nh@gmail.com>
2025-11-30 15:03:25 -0300

    fix images projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,35 @@
+import pythonJsImage from '../assets/images/python-javascript.png'
+import matematicaImage from '../assets/images/matematica.png'
+import javaGitImage from '../assets/images/java-git.png'
+import arquitecturaImage from '../assets/images/arquitectura.png'
+import organizacionImage from '../assets/images/organizacion.png'
+
 export const projects = [
   {
     id: 1,
     title: 'Python & Javascript',
-  image: 'src/assets/images/python-javascript.png',
+    image: pythonJsImage,
     link: 'https://drive.google.com/drive/folders/1XvCxydcIeKWRKGAKpFq6UBkihHomx7oq?usp=sharing',
     position: 'left'
   },
   {
     id: 2,
     title: 'Matemática',
-  image: 'src/assets/images/matematica.png',
+    image: matematicaImage,
     link: 'https://drive.google.com/drive/folders/1m7535y8wLPugtES1qLu7IJKmviJ1IAop?usp=sharing',
     position: 'right'
   },
   {
     id: 3,
     title: 'Java & Git',
-  image: 'src/assets/images/java-git.png',
+    image: javaGitImage,
     link: 'https://drive.google.com/drive/folders/14Y6QaODAeBQr3U3CeFi_2ryFxTQOhmGx?usp=sharing',
     position: 'left'
   },
   {
     id: 4,
     title: 'Arquitectura y Sistemas Operativos',
-  image: 'src/assets/images/arquitectura.png',
+    image: arquitecturaImage,
     link: 'https://drive.google.com/drive/folders/1e0PzoNla14dT0gnKqh78bA2axqOX8vRb?usp=sharing',
     position: 'right',
     smallMargin: true
@@ -31,7 +37,7 @@ export const projects = [
   {
     id: 5,
     title: 'Organización Empresarial',
-  image: 'src/assets/images/organizacion.png',
+    image: organizacionImage,
     link: 'https://drive.google.com/drive/folders/1JCP6byRYzs_YEwx4-_DHL3CxhliL-9YW?usp=sharing',
     position: 'left',
     smallMargin: true
