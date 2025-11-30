b09f0108e066dad9e94dca1a634a51df2684f41b
RodriHaro <rodri.nh@gmail.com>
2025-11-30 14:57:25 -0300

    Fix images v2

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -1,37 +1,43 @@
+import franImage from '../assets/images/fran.jpeg'
+import rodrigoImage from '../assets/images/image.png'
+import andreImage from '../assets/images/andre.jpeg'
+import joelImage from '../assets/images/joel.jpeg'
+import gabrielImage from '../assets/images/gabriel.jpeg'
+
 export const teamMembers = [
   {
     id: 1,
     name: 'Francisco José Martín',
     location: 'San Martín, Mendoza',
-  image: '../../../src/assets/images/fran.jpeg',
+    image: franImage,
     github: 'https://github.com/fran-martin713'
   },
   {
     id: 2,
     name: 'Rodrigo Haro',
     location: 'San Rafael, Mendoza',
-  image: '../../../src/assets/images/image.png',
+    image: rodrigoImage,
     github: 'https://github.com/RodriHaro'
   },
   {
     id: 3,
     name: 'Andrea Vargas',
     location: 'San Rafael, Mendoza',
-  image: '../../../src/assets/images/andre.jpeg',
+    image: andreImage,
     github: 'https://github.com/avv2025'
   },
   {
     id: 4,
     name: 'Joel Francisco Martín',
     location: 'San Martín, Mendoza',
-  image: '../../../src/assets/images/joel.jpeg',
+    image: joelImage,
     github: 'https://github.com/joelmartin73'
   },
   {
     id: 5,
     name: 'Gabriel Jauregui',
     location: 'Tupungato, Mendoza',
-  image: '../../../src/assets/images/gabriel.jpeg',
+    image: gabrielImage,
     github: 'https://github.com/GabiJauregui10'
   }
 ]
